ae1ce501aef64cb7b8c9e6b0533d6169239a3ed3
wlonep <frin6189@gmail.com>
2025-04-10 14:06:24 +0900

    미완성

---
+++ MyNode.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class MyNode {
+    private Object element;
+    private MyNode parent;
+    private ArrayList<MyNode> children;
+
+    MyNode() {
+        element = null;
+        parent = null;
+        children = new ArrayList<>();
+    }
+    MyNode(Object e) {
+        element = e;
+        parent = null;
+        children = new ArrayList<>();
+    }
+    public Object element() {
+        return element;
+    }
+    public MyNode parent() {
+        return parent;
+    }
+    public ArrayList<MyNode> children() {
+        return children;
+    }
+    public int degree() {
+        int max = children.size();
+        for (MyNode child : children) {
+            max = Math.max(max, child.degree());
+        }
+        return max;
+    }
+    public void setParent(MyNode p) {
+        parent = p;
+    }
+
+    public void setElement(Object e) {
+        element = e;
+    }
+    public void setChildren(ArrayList<MyNode> c) {
+        children = c;
+    }
+}
---
+++ MyTree.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+
+public class MyTree {
+    private MyNode root;
+
+    // implement constructors
+    MyTree() {
+        root = null;
+    }
+
+    MyTree(Object e) {
+        root = new MyNode(e);
+        root.setChildren(new ArrayList<>());
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(MyNode v) {
+        if (v == null) return 0;
+        int count = 1;
+        for (MyNode child : children(v)) {
+            count += size(child);
+        }
+        return count;
+    }
+
+    public MyNode root() {
+        return root;
+    }
+
+    public ArrayList<MyNode> children(MyNode v) {
+        return v.children();
+    }
+
+    public boolean isExternal(MyNode v) {
+        return v.children().isEmpty();
+    }
+
+    public MyNode addRoot(Object e) {
+        if (root != null) return null;
+        root = new MyNode(e);
+        root.setChildren(new ArrayList<>());
+        return root;
+    }
+
+    public MyNode addNode(Object e) {
+        MyNode temp = new MyNode(e);
+        temp.setChildren(new ArrayList<>());
+        return temp;
+    }
+
+    public MyNode addChild(MyNode v, Object e) {
+        MyNode child = new MyNode(e);
+        child.setChildren(new ArrayList<>());
+        child.setParent(v);
+        v.children().add(child);
+        return child;
+    }
+
+    public MyNode addChild(MyNode v, int i, Object e) {
+        MyNode child = new MyNode(e);
+        child.setChildren(new ArrayList<>());
+        child.setParent(v);
+        v.children().add(i, child);
+        return child;
+    }
+
+    public MyNode setChild(MyNode v, int i, Object e) {
+        MyNode child = v.children().get(i);
+        child.setElement(e);
+        return child;
+    }
+
+    public MyNode removeChild(MyNode v, int i) {
+        return v.children().remove(i);
+    }
+}
